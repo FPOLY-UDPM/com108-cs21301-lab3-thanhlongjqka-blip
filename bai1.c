309043f595b97c48d98f51a3d83977602b387d28
thanhlongjqka-blip <thanhlongjqka@gmail.com>
2026-01-17 13:02:08 +0530

    nộp LAB3

--- bai1.c
+++ bai1.c
@@ -18,6 +18,27 @@
 #include <stdio.h>
 
 int main(){
+        float diem;
+    printf("Nhập điểm số: ");
+    scanf("%f", &diem);
+    if(diem<0 || diem>10)
+    printf("không có kết quả");
+    else if(diem>=9)
+    printf("Học lực xuất sắc");
+   else if(diem>=9)
+    printf("Học lực xuất sắc");
+  else if(diem>=8)
+    printf("Học lực giỏi");
+      else if(diem>=6.5)
+    printf("Học lực khá");
+      else if(diem>=5)
+    printf("Học lực trung bình");
+      else if(diem>=3.5)
+    printf("Học lực yếu");
+    else
+    printf("Học lực kém");
+
+
 
     // Khai báo biến
 
--- bai2_2.c
+++ bai2_2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Thanh Long]
+ * MSSV:      [PS49311]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH GIẢI PHƯƠNG TRÌNH  
@@ -20,9 +20,36 @@
 // o X2 = (-b – căn(delta))/(2*a)
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
+    float a, b, c, delta, X1, X2, X;
+    printf("Nhập giá trị a: ");
+    scanf("%f", &a);
+     printf("Nhập giá trị b: ");
+    scanf("%f", &b);
+     printf("Nhập giá trị c: ");
+    scanf("%f", &c);
+    if(a==0)
+    printf("Phương trình trở thành b*x + c = 0");
+    else if(a!=0)
+    {
+        delta = b*b - 4*a*c;
+        if(delta<0)
+        printf("Phương trình vô nghiệm");
+        else if(delta==0)
+        {
+            X = -b/(2*a);
+            printf("Phương trình có nghiệm kép x = %.2f", X);
+        }
+        else if(delta>0)
+        {
+            X1 = (-b + sqrt(delta))/(2*a);
+            X2 = (-b - sqrt(delta))/(2*a);
+            printf("Phương trình có 2 nghiệm phân biệt:\nX1 = %.2f\nX2 = %.2f", X1, X2);
+        }
     
+    }
     // Khai báo biến
 
 
